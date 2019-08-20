d2528f0b4f4543782b44454dbc0ed350f700c621
Ant Henderson <anthenderson89@gmail.com>
2019-08-20 21:30:17 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCLdMgKE0h0KtQgQc7-vmX6BGnRww23ns4",
-    authDomain: "henderson-home-iot.firebaseapp.com",
-    databaseURL: "https://henderson-home-iot.firebaseio.com",
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXXX",
     projectId: "henderson-home-iot",
     storageBucket: "henderson-home-iot.appspot.com",
     messagingSenderId: "11108305568"
